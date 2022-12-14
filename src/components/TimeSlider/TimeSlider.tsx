75bb9c869cbf8f307163ea7a2f239987da973085
Xinzhe Chai <xinzhe_chai@brown.edu>
2022-12-14 14:23:53 -0500

    improved slider with on/off controll

--- src/components/TimeSlider/TimeSlider.tsx
+++ src/components/TimeSlider/TimeSlider.tsx
@@ -1,7 +1,7 @@
-import { Slider, SliderTrack, SliderFilledTrack, SliderThumb, Box } from "@chakra-ui/react";
+import { Slider, SliderTrack, SliderFilledTrack, SliderThumb, Box, Button } from "@chakra-ui/react";
 import { SunIcon } from '@chakra-ui/icons'
 import "./styles.css"
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface TimerProps {
 	time: number,
@@ -11,24 +11,44 @@ interface TimerProps {
 export const TimeSlider = (props: TimerProps) => {
 
 	const { time, setTime } = props
+	const [isAnimated, setIsAnimated] = useState(true)
+
 	let realSec = useRef(0);
 
-	// useEffect(() => {
-	// 	const id = setInterval(() => {
-	// 		// console.log(time)
-	// 		setTime(realSec.current + 0.05)
-	// 		realSec.current += 0.05
-	// 	}, 100);
-	// 	// clean up after component unmount
-	// 	return () => clearInterval(id);
-	// }, []);
+	useEffect(() => {
+		let autoIncrementor: NodeJS.Timeout | undefined;
+		if (isAnimated) {
+			autoIncrementor = setInterval(() => {
+				// console.log(time)
+				setTime(realSec.current + 0.05)
+				realSec.current += 0.05
+				if (realSec.current >= 24) {
+					setTime(0)
+					realSec.current = 0
+				}
+			}, 20);
+		} else {
+			clearInterval(autoIncrementor)
+		}
+		// clean up after component unmount
+		return () => clearInterval(autoIncrementor);
+	}, [isAnimated]);
+
+	const handleSliderChange = (e: number) => {
+		setTime(e)
+		realSec.current = e
+	}
+
+	const handleAnimation = () => {
+		setIsAnimated(!isAnimated)
+	}
 
 	return (
 		<div className="time-wrapper">
 			<div className="time-text">
 				time of the day
 			</div>
-			<Slider aria-label='slider-ex-4' min={0} max={24} value={time} step={0.01} onChange={(e) => setTime(e)}>
+			<Slider aria-label='slider-ex-4' min={0} max={24} value={time} step={0.01} onChange={handleSliderChange}>
 				<SliderTrack bg='darkgray'>
 					<SliderFilledTrack bg='gray' />
 				</SliderTrack>
@@ -36,6 +56,9 @@ export const TimeSlider = (props: TimerProps) => {
 					<Box color='black' as={SunIcon} />
 				</SliderThumb>
 			</Slider>
+			<Button variant="outline" size='xs' width="100%" marginTop={2} onClick={handleAnimation}>
+				{`${isAnimated ? "stop" : "start"} animation`}
+			</Button>
 		</div>
 	)
 }
